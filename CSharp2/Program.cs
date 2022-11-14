b654383d752ab290b9f4058bb2bf5c648ac5217c
tajulislam <tajulislam8028@gmail.com>
2022-11-14 22:00:13 +0600

    added text.txt

---
+++ CSharp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CSharp2
+{
+    class Program
+    {
+        public static int fibo(int num)
+        {
+            if(num==0)
+            {
+                return 0;
+            }
+            else if(num==1||num==2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibo(num-2)+fibo(num-1);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        //     int Number1,Number2;
+        //    Console.Write("Please Enter Your Number1:");
+        //      Number1=int.Parse(Console.ReadLine());
+        //    Console.Write("Please Enter Your Number2:");
+        //      Number2=int.Parse(Console.ReadLine());
+        //    Console.Write(Number1+"*"+Number2+"="+Number1*Number2);
+        Console.Write("Please Enter Your Number:");
+        int i,num,count=0;
+        num=int.Parse(Console.ReadLine());
+        //Console.Write(num);
+        for(i=2;i<=Math.Sqrt(num);i++)
+        {
+           if(num%i==0)
+             count++;
+        }
+         if(count==0 && num>1)
+         {
+        Console.Write("\nPrime");
+
+         }
+         else
+         {
+        Console.Write("Not Prime");
+             
+         }
+        // }
+        //   int i,num;
+
+        // Console.Write("Please Enter a number:");
+        // num=int.Parse(Console.ReadLine());
+
+        //   for(i=0;i<num;i++)
+        //   {
+              
+        //     Console.Write("\t"+fibo(i));
+
+        //   }
+    }
+}
+}
